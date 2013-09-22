d1eb9a4a391f2f71ee1b144eadb99d41434a8b11
shafiry <yuvalshafir@yahoo.com>
2013-09-22 09:30:20 -0400

    added twilio

--- app/models/Incident.php
+++ app/models/Incident.php
@@ -16,8 +16,30 @@ class Incident extends Eloquent {
 
 	public static function add_incident($title, $description, $coordinates){
        		$incident = Incident::create(array('title' => $title, 'description' => $description, 'dispatcher_id' => Session::get('user')->id, 'coordinates' => $coordinates));
+
+
+		$volunteers = array();
+		foreach($incident->dispatcher->volunteers as $volunteer)
+			{
+				$volunteers[$volunteer->phone] = $volunteer->name;
+			}
+		
+		$AccountSid = "AC26eaf500ebb246cefb61e96834aba585";
+                $AuthToken = "37529e67f84db998192627ca8a7eb219";
+
+		$client = new Services_Twilio($AccountSid, $AuthToken);
+ 		foreach ($volunteers as $phone => $name) {
+ 
+		       	$sms = $client->account->sms_messages->create(
+                        "702-728-3363", // From this number
+                        $phone, // To this number
+                        "!"
+                        );
+
+ 
+	        // Display a confirmation message on the screen
+       		 echo "Sent message to $name";
+   		 }		
        }
-
-
 }
 
