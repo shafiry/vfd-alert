a4ccac1fb771a49fbf93418615ebba5cd2bd2b4b
Daniel O'Connor <daniel@danoc.me>
2013-09-22 08:31:25 -0400

    create beginning of dashboard page

--- app/views/pages/dashboard.blade.php
+++ app/views/pages/dashboard.blade.php
@@ -1,11 +1,15 @@
 @extends('master')
 
 @section('container')
-	<p>Dashboard....</p>
-        {{ Form::open(array('url' => '/volunteer/create')) }}
-        {{ Form::text('name') }}
-        {{ Form::text('phone', "xxx-xxx-xxxx") }}
-        {{ Form::submit('New Volunteer!') }}
-        {{ Form::close() }}
-	
+	<div class="incidents">
+		<h2 class="section-title">Incidents</h2>
+	</div>
+	<div class="volunteers">
+		<h2 class="section-title">Volunteers</h2>
+		{{ Form::open(array('url' => '/volunteer/create')) }}
+		{{ Form::text('name') }}
+		{{ Form::text('phone', "xxx-xxx-xxxx") }}
+		{{ Form::submit('New Volunteer!') }}
+		{{ Form::close() }}
+	</div>	
 @stop
